d05adfeae273f49dc853ae0f5fd9c53ff0741371
klas gudmundsson <klagu863@student.liu.se>
2018-12-04 18:45:46 +0100

    Add finished task 3b

--- 3/3b.py
+++ 3/3b.py
@@ -4,23 +4,41 @@ f = open('fabrics.txt', 'r')
 
 fabricArray = [[0 for x in range(1000)] for y in range(1000)]
 
-for line in f:
-    regexString = r'^#\d{1,4} @ (\d{1,3}),(\d{1,3}): (\d{1,3})x(\d{1,3})'
-    m = re.search(regexString, line)
-    xCoord = int(m.group(1))
-    yCoord = int(m.group(2))
-    xWidth = int(m.group(3))
-    yWidth = int(m.group(4))
+def firstLoop(file, arr):
+    for line in file:
+        regexString = r'^#(\d{1,4}) @ (\d{1,3}),(\d{1,3}): (\d{1,3})x(\d{1,3})'
+        m = re.search(regexString, line)
+        id = int(m.group(1))
+        xCoord = int(m.group(2))
+        yCoord = int(m.group(3))
+        xWidth = int(m.group(4))
+        yWidth = int(m.group(5))
+        
+        for x in range (0, xWidth):
+            for y in range (0, yWidth):
+                arr[x + xCoord][y + yCoord] += 1
 
-    for x in range (0, xWidth):
-        for y in range (0, yWidth):
-            fabricArray[x + xCoord][y + yCoord] += 1
+    return arr
 
-patchesWithCountLargerThanOne = 0
-for x in range (0, 1000):
-        for y in range (0, 1000):
-            if fabricArray[x][y] > 1:
-                patchesWithCountLargerThanOne += 1
+def secondLoop(file, arr):
+    for line2 in file:
+        regexString = r'^#(\d{1,4}) @ (\d{1,3}),(\d{1,3}): (\d{1,3})x(\d{1,3})'
+        m = re.search(regexString, line2)
+        id = int(m.group(1))
+        xCoord = int(m.group(2))
+        yCoord = int(m.group(3))
+        xWidth = int(m.group(4))
+        yWidth = int(m.group(5))
+        
+        notOverlapping = True
+        for x in range (0, xWidth):
+            for y in range (0, yWidth):
+                if(arr[x + xCoord][y + yCoord] != 1):
+                    notOverlapping = False
+        if (notOverlapping):
+            print id
 
-print patchesWithCountLargerThanOne
+fabricArray = firstLoop(f,fabricArray)
+f.seek(0) #Reset the file's current position, at this point the pointer points to the end of the file.
+secondLoop(f,fabricArray)
 f.close()
